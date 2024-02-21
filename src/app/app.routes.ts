88001d80525f8997afed854046e86558237d5cda
marvinkn <marvin.nyingi97@gmail.com>
2024-02-21 10:48:10 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent,
+  },
+  {
+    path: 'transactions',
+    component: TransactionsComponent,
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
+  },
+];
