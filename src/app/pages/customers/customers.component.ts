a82cc4d5a548160c8c5de536f50b2c02c664bdba
marvinkn <marvin.nyingi97@gmail.com>
2024-02-22 15:41:52 +0300

    simplified dashboard and customers page layout

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+  styleUrl: './customers.component.css',
 })
 export class CustomersComponent {
-
+  email = 'foo@example.com';
 }
