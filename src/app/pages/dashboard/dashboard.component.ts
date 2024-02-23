c770fdf6b410d0996ad4bf7d32d3f989ca0a28a1
marvinkn <marvin.nyingi97@gmail.com>
2024-02-23 12:19:48 +0300

    successfully integrated backend api

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
+import { Customer } from '../../models/Customer';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   email = 'johndoe@example.com';
+  customers: Customer[] = [];
+
+  private customerService = inject(CustomerService);
+
+  ngOnInit(): void {
+    this.customerService.getAllCustomers().subscribe(
+      (data: Customer[]) => {
+        this.customers = data;
+        // Handle the received data here
+      },
+      (error) => {
+        console.error('Error fetching customers:', error);
+        // Handle error here
+      }
+    );
+  }
 }
