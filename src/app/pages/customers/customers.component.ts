294f69c9c1b69fe0bd0aeba50de776c3f4d919e4
marvinkn <marvin.nyingi97@gmail.com>
2024-02-23 13:32:28 +0300

    consumed getAllCustomers endpoint and populated customers table with relevant data

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Customer } from '../../models/Customer';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-customers',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css',
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
   email = 'foo@example.com';
+  customers: Customer[] = [];
+  loading = false;
+
+  private customerService = inject(CustomerService);
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.customerService.getAllCustomers().subscribe(
+      (data: Customer[]) => {
+        this.customers = data;
+        this.loading = false;
+        // Handle the received data here
+      },
+      (error) => {
+        console.error('Error fetching customers:', error);
+        // Handle error here
+      }
+    );
+  }
 }
